49109aa5535ef82144f69b300d4f37dd893b56b3
Bitcoin Eagle <hello@bitcoineagle.dev>
2023-05-25 12:12:02 +0800

    #5 introduce ChainStateEvent enum

--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -1,1 +1,10 @@
-
+pub enum ChainStateEvent {
+    /// Spendable: UTXO has been confirmed in a block in a best chain
+    UtxoConfirmed {},
+    /// Not-Spendable: UTXO has been reorged out of current best chain after it had been previously confirmed
+    UtxoUnconfirmed {},
+    /// Not-Spendable: UTXO spend has been confirmed in a block in a best chain
+    UtxoSpent {},
+    /// Spendable: UTXO spend has been reorged out of current best chain after it had been previously confirmed spent
+    UtxoUnspent {},
+}
